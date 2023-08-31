e8cec7624dbc24232ef7f882d2935c7ab0d56d10
Andrés Felipe Salinas Martínez <andresali1@hotmail.es>
2023-08-31 14:01:27 -0500

    Agregando primer proyecto en Angular

---
+++ Angular/Basico/PrimerApp/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmpleadosComponent } from './empleados/empleados.component';
+import { EmpleadoComponent } from './empleado/empleado.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmpleadosComponent,
+    EmpleadoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Angular/Basico/PrimerApp/src/app/empleado/empleado.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empleado',
+  templateUrl: './empleado.component.html',
+  //template: "<p>Aqui iría un empleado</p>", (Html en línea)
+  styleUrls: ['./empleado.component.css']
+  //styles: ["p{background-color: aquamarine;}"] (Estilos en línea)
+})
+export class EmpleadoComponent {
+  //nombre = 'Lorena';
+  apellido = 'Gómez';
+  edad = 18;
+  //empresa = 'Tech S.A.S.';
+
+  //Propiedades para binding
+  inhabilitacionEmpresa = false;
+  estaRegistrado = false;
+  textoDeRegistro = "No hay nadie registrado";
+
+  registroUsuario(value:boolean){
+    this.estaRegistrado = value;
+    if (this.estaRegistrado){
+      this.crearRegistro();
+    } else {
+      this.removerRegistro();
+    }
+  }
+
+  guardaEmpresa(value:string){
+    var tieneTexto = value != ''
+    this.registroUsuario(tieneTexto);
+  }
+
+  validarRegistrado(event:Event){
+    if((<HTMLInputElement>event.target).value == "Si"){
+      this.crearRegistro();
+    } else {
+      this.removerRegistro();
+    }
+  }
+
+  crearRegistro(){
+    this.estaRegistrado = true;
+    this.textoDeRegistro = "Se ha registrado un usuario";
+  }
+
+  removerRegistro(){
+    this.estaRegistrado = false;
+    this.textoDeRegistro = "No hay nadie registrado";
+  }
+}
