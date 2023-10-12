aa9373b5a19ceff963aee5b81b05f88777941e58
Andrés Felipe Salinas Martínez <andresali1@hotmail.es>
2023-10-12 17:02:36 -0500

    Agregando estructura de proyecto

--- Angular/Ejercicios/control-clientes/src/app/app-routing.module.ts
+++ Angular/Ejercicios/control-clientes/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TableroComponent } from './components/tablero/tablero.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { ConfiguracionComponent } from './components/configuracion/configuracion.component';
+import { EditarClienteComponent } from './components/editar-cliente/editar-cliente.component';
+import { NoEncontradoComponent } from './components/no-encontrado/no-encontrado.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TableroComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'registro', component: RegistroComponent },
+  { path: 'configuracion', component: ConfiguracionComponent },
+  { path: 'cliente/editar/:id', component: EditarClienteComponent },
+  { path: '**', component: NoEncontradoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
